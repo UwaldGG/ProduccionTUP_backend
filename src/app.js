f8c063f1d312aa289bdb2046f00ce87551345e67
Uwaldo-Villatoro <uwualdoleo@gmail.com>
2024-09-16 19:00:22 -0600

    Base de backend

---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req,res) => {
+    res.send('backen con nodejs');
+});
+
+app.listen(port,()=>{
+    console.log("Port ==> ", port);
+});
